280b8ffe40d971888b80ec23f43e8ea665842e2c
Claudio Segala Rodrigues Silva Filho <claudiosegalafilho@gmail.com>
2018-04-30 11:31:49 -0300

    Added the definition of the Reader

---
+++ src/lib/reader.hpp
@@ -0,0 +1,46 @@
+#include <bit/stdc++.h>
+
+class ByteCodeReader {
+	private:
+	
+	//< stream with the .class file
+	ifstream _class;
+	
+	/**
+	* @brief  Check if it is a .class file
+	* @retval TRUE Has CAFEBABE as initial word in the file
+	* @retval FALSE Has not CAFEBABE as initial word in the file
+	*/
+	bool isValid ();
+	
+	public:
+	
+	/**
+	* @brief  Open a .class file
+	* @param  filename The name of the .class we are opening
+	*/
+	void open (string filename);
+	
+	/**
+	* @brief  Get the next byte in the file
+	* @retval The next Byte
+	*/
+	Byte getNextByte ();
+	
+	/**
+	* @brief  Get the next half word in the file
+	* @retval The next Half Word
+	*/
+	HalfWord getNextHalfWord ();
+	
+	/**
+	* @brief  Get the next word in the file
+	* @retval The next Word
+	*/
+	Word getNextWord ();
+	
+	/**
+	* @brief  Close the .class file
+	*/
+	void close ();
+};
