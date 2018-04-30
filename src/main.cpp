97039f395c83ebacaf8637bb9b7123c2de56cd3d
Claudio Segala Rodrigues Silva Filho <claudiosegalafilho@gmail.com>
2018-04-30 11:33:30 -0300

    Added libraries

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
-
-using namespace std;
+#include "lib/bit.hpp"
+#include "lib/reader.hpp"
 
 int main () {
 	int size = sizeof(void *);
-	cout << "Size: " << size << endl;
-	cout << (size == 4 ? "32-bit" : "64-bit") << endl;
+	std::cout << "Size: " << size << endl;
+	std::cout << (size == 4 ? "32-bit" : "64-bit") << endl;
 	return 0;
 }
