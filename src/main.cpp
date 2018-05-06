dc9b9d0de5cb5e5dccc7a650343c598e74304b41
Ricardo Kury <ricardokury@outlook.com>
2018-05-06 03:36:59 -0300

    main() now demonstrates Data class functionality

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,14 @@
-#include <iostream>
-#include "lib/bit.hpp"
-#include "lib/reader.hpp"
+#include <cstdio>
+#include "bit.hpp"
 
 int main () {
-	int size = sizeof(void *);
-	std::cout << "Size: " << size << endl;
-	std::cout << (size == 4 ? "32-bit" : "64-bit") << endl;
-	return 0;
+    jvm::Data<uint32_t> w;
+    w = 0x1234ABCD;
+    w.Print();
+    w.toEndianness(jvm::Endianness::BIG);
+    w.Print();
+    w.toEndianness(jvm::Endianness::LITTLE);
+    w.Print();
+    getchar();
+    return 0;
 }
