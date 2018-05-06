aa2848a74bbfcc887f8d724dce66e7b01bd835a5
Lincoln <lincoln_abreu@aluno.unb.br>
2018-05-06 19:48:56 +0000

    incluido primeira versão do utf8<->utf16

--- src/utf8_utf16.cpp
+++ src/utf8_utf16.cpp
@@ -5,13 +5,16 @@
 namespace jvm;
 
 //UTF-8 to UTF-16 
-jvm::string source; 
-//... 
-std::wstring_convert<std::codecvt_utf8_utf16<char16_t>,char16_t> convert; 
-jvm::u16string dest = convert.from_bytes(source);     
- 
+jvm::u16string utf8_to_utf16(const jvm::string source){
+	std::wstring_convert<std::codecvt_utf8_utf16<char16_t>,char16_t> convert; 
+
+	jvm::u16string dest = convert.from_bytes(source);     
+	return dest;
+}
+
 //UTF-16 to UTF-8 
-jvm::u16string source; 
-//... 
-std::wstring_convert<std::codecvt_utf8_utf16<char16_t>,char16_t> convert; 
-jvm::string dest = convert.to_bytes(source);     
+jvm::string utf16_to_utf8(const jvm::u16string source){ 
+	std::wstring_convert<std::codecvt_utf8_utf16<char16_t>,char16_t> convert; 
+	jvm::string dest = convert.to_bytes(source);     
+	return dest;
+}
