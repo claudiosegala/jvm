c95b925f4100e4a79436e048c43889dc0b680801
lightguy875 <luz875@gmail.com>
2018-05-06 16:41:06 -0300

    Argc e Argv adicionados

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,17 @@
 #include "lib/bit.hpp"
 #include "lib/reader.hpp"
 
-int main () {
+int main (int argc, char *argv[ ]) {
 	int size = sizeof(void *);
 	std::cout << "Size: " << size << endl;
 	std::cout << (size == 4 ? "32-bit" : "64-bit") << endl;
+
+	if (argc < 2)
+    {
+        std::cout << "Numero de argumentos insuficientes" << endl;
+    }
+
+
+
 	return 0;
 }
